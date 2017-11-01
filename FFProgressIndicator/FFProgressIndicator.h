558f76cb46729cccccebac46ab9edb2c66ae7e26
Ujwal <>
2017-11-01 19:02:08 +0530

    Project Dependencies Added

---
+++ FFProgressIndicator/FFProgressIndicator.h
@@ -0,0 +1,19 @@
+//
+//  FFProgressIndicator.h
+//  FFProgressIndicator
+//
+//  Created by Ujwal on 11/1/17.
+//  Copyright © 2017 Fastfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFProgressIndicator.
+FOUNDATION_EXPORT double FFProgressIndicatorVersionNumber;
+
+//! Project version string for FFProgressIndicator.
+FOUNDATION_EXPORT const unsigned char FFProgressIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFProgressIndicator/PublicHeader.h>
+
+
